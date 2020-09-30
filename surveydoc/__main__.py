e6d20ef8dc27ab17db71e92b00caca3f4808f8ac
Jason Bailey <accounts@sidewaysgravity.com>
2020-09-30 11:40:42 -0500

    Removing extra line breaks

--- surveydoc/__main__.py
+++ surveydoc/__main__.py
@@ -43,10 +43,8 @@ def main(credentials_path, config_path):
                 answers = recent_responses.filter(survey_results['answers']['Timestamp'], survey_results['answers'][question])
                 doc_writer.text_summary(question, answers)
 
-
         document_id = doc_writer.generate_doc("{} {}".format(datetime.now().strftime("%Y-%m"), subject_config['name']))
         drive_manager.move_doc_to_folder(document_id, subject_config['drive-folder'])
-
 
 if __name__ == '__main__':
     main()
